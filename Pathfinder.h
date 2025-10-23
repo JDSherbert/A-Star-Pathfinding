66649dfb45e67c3b5d38b91d29c32584a572f625
JDSherbert <43964243+JDSherbert@users.noreply.github.com>
2025-10-23 01:05:13 +0100

    Update Pathfinder.h

--- Pathfinder.h
+++ Pathfinder.h
@@ -2,23 +2,22 @@
 #pragma once
 
 #include <vector>
-#include <queue>
-#include <cmath>
 
 // Define a structure to represent a node in the graph
 struct Node
 {
     int x, y; // Coordinates of the node in the graph
-    int f, g, h; // Values used by the A* algorithm
+    int f, g, h; // Cost values used by the A* algorithm
 
-    Node(int r, int c);
+    Node(int _x = 0, int _y = 0);
 
     // Overload comparison operators for priority queue
     bool operator>(const Node& other) const;
     bool operator==(const Node& other) const;
 };
 
-// A* pathfinding algorithm
+// Finds a path from start to goal using the A* algorithm.
+// `graph` should be a 2D grid where 0 = walkable, 1 = blocked.
 std::vector<Node> FindPath(const std::vector<std::vector<int>>& graph, const Node& start, const Node& goal);
 
 // Function to print the path
